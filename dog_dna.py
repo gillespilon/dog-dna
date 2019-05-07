affb2caf0cf2c13410054fb083e395695b9d72df
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-05-07 08:20:19 -0500

    Sync .py, .ipynb; fix display output in .py; replace inplace=True.

--- dog_dna.py
+++ dog_dna.py
@@ -11,17 +11,30 @@ Dog DNA
 import pandas as pd
 
 
-FILE_RAW = 'dog_dna.csv'
-df = pd.read_csv(FILE_RAW)
+pd.options.display.max_rows = None
+pd.options.display.max_columns = None
+pd.set_option('display.width', 1000)
 
-print(pd.crosstab(df['Dog 1 A'], df['Dog 1 G']), '\n')
-print(pd.crosstab(df['Dog 1 A'], df['Dog 1 G'],
+FILE_RAW = 'dog_dna_raw.csv'
+FILE_MUNGED= 'dog_dna_munged.csv'
+USECOLS = ['Dog 1 A', 'Dog 1 G', 'Dog 2 A', 'Dog 2 G']
+df = pd.read_csv(FILE_RAW, usecols=USECOLS)
+
+
+labels = df.columns
+df['Dog1'] = df['Dog 1 A'] + df['Dog 1 G']
+df['Dog2'] = df['Dog 2 A'] + df['Dog 2 G']
+df = df.drop(columns=labels)
+df.to_csv(FILE_MUNGED)
+
+
+print(pd.crosstab(df['Dog1'], df['Dog2']), '\n')
+print(pd.crosstab(df['Dog1'], df['Dog2'],
       margins=True,
       margins_name='Total',
       dropna=True), '\n')
-print(pd.crosstab(df['Dog 1 A'], df['Dog 1 G'],
+print(pd.crosstab(df['Dog1'], df['Dog2'],
       margins=True,
       margins_name='Total',
       dropna=True,
       normalize=True,).round(4)*100)
-
